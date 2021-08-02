5bb8946a46872b65cf68cb39a4d9c08407621369
TmLev <sololev27@gmail.com>
2021-08-02 19:27:46 +0300

    refactor: Update main

--- rust/ray-traycer/src/bin/create_ppm.rs
+++ rust/ray-traycer/src/bin/create_ppm.rs
@@ -5,7 +5,8 @@ use std::{
 
 use anyhow::Result;
 
-use ray_traycer::{Point3, Ray, Vec3};
+use ray_traycer::{Hittable, HittableList, Point3, Ray, Sphere, Vec3};
+use std::rc::Rc;
 
 const OUTPUT_PATH: &str = "image.ppm";
 
@@ -16,7 +17,11 @@ const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as usize;
 type Color = Vec3;
 
 fn main() -> Result<()> {
+    // Logging
+
     env_logger::init();
+
+    // Output
 
     let file = File::create(OUTPUT_PATH)?;
     let mut output = BufWriter::new(file);
@@ -24,6 +29,14 @@ fn main() -> Result<()> {
     writeln!(&mut output, "P3")?;
     writeln!(&mut output, "{} {}", IMAGE_WIDTH, IMAGE_HEIGHT)?;
     writeln!(&mut output, "255")?;
+
+    // World
+
+    let mut world = HittableList::default();
+    world.add(Rc::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    world.add(Rc::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+
+    // Camera
 
     let viewport_height = 2.0;
     let viewport_width = ASPECT_RATIO * viewport_height;
@@ -35,6 +48,8 @@ fn main() -> Result<()> {
     let lower_left_corner =
         origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
+    // Render
+
     for row in (0..IMAGE_HEIGHT).rev() {
         log::info!("Lines remaining: {}/{}", row + 1, IMAGE_HEIGHT);
 
@@ -45,7 +60,7 @@ fn main() -> Result<()> {
                 origin,
                 lower_left_corner + horizontal * u + vertical * v - origin,
             );
-            write_color(&mut output, ray_color(&ray));
+            write_color(&mut output, ray_color(&ray, &world));
         }
     }
 
@@ -65,8 +80,13 @@ fn write_color(output: &mut impl Write, color: Color) {
     .unwrap();
 }
 
-fn ray_color(ray: &Ray) -> Color {
-    let direction = ray.direction().to_unit();
-    let t = 0.5 * (direction.y() + 1.0);
-    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+fn ray_color(ray: &Ray, world: &impl Hittable) -> Color {
+    match world.hit(ray, 0.0, f64::INFINITY) {
+        Some(record) => (*record.normal() + Color::new(1.0, 1.0, 1.0)) * 0.5,
+        None => {
+            let direction = ray.direction().to_unit();
+            let t = 0.5 * (direction.y() + 1.0);
+            Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+        }
+    }
 }
