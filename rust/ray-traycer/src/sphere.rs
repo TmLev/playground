d2a512fe70888884fd23769eecfe481bebb130d7
TmLev <sololev27@gmail.com>
2021-08-02 19:27:10 +0300

    feat: Implement `Sphere`

---
+++ rust/ray-traycer/src/sphere.rs
@@ -0,0 +1,44 @@
+use crate::{HitRecord, Hittable, Point3, Ray};
+
+#[derive(Copy, Clone, Default)]
+pub struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: Point3, radius: f64) -> Self {
+        Self { center, radius }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let oc = *ray.origin() - self.center;
+
+        let a = ray.direction().length_squared();
+        let half_b = oc.dot(ray.direction());
+        let c = oc.length_squared() - self.radius.powi(2);
+
+        let discriminant = half_b.powi(2) - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let sqrtd = discriminant.sqrt();
+        let mut root = (-half_b - sqrtd) / a;
+
+        if root < t_min || root > t_max {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || root > t_max {
+                return None;
+            }
+        }
+
+        let point = ray.at(root);
+        let normal = (point - self.center) / self.radius;
+        let mut record = HitRecord::new(point, normal, root);
+        record.set_face_normal(ray, &normal);
+        Some(record)
+    }
+}
