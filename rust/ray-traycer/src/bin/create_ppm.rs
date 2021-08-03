38889a3641935ff5de61a48e895204ed180079a0
TmLev <sololev27@gmail.com>
2021-08-03 19:43:27 +0300

    chore: Bump main

--- rust/ray-traycer/src/bin/create_ppm.rs
+++ rust/ray-traycer/src/bin/create_ppm.rs
@@ -32,9 +32,9 @@ fn main() -> Result<()> {
     let mut world = HittableList::default();
 
     let material_ground = Rc::new(materials::Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(materials::Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let material_left = Rc::new(materials::Metal::new(Color::new(0.8, 0.8, 0.8)));
-    let material_right = Rc::new(materials::Metal::new(Color::new(0.8, 0.6, 0.2)));
+    let material_center = Rc::new(materials::Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(materials::Dielectric::new(1.5));
+    let material_right = Rc::new(materials::Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
     world.add(Rc::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
@@ -49,6 +49,11 @@ fn main() -> Result<()> {
     world.add(Rc::new(Sphere::new(
         Point3::new(-1.0, 0.0, -1.0),
         0.5,
+        material_left.clone(),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        -0.4,
         material_left,
     )));
     world.add(Rc::new(Sphere::new(
