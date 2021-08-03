f69de46100917ffab92c97ab212991838dbf79d9
TmLev <sololev27@gmail.com>
2021-08-03 13:01:33 +0300

    feat: Introduce `camera`

---
+++ rust/ray-traycer/src/camera.rs
@@ -0,0 +1,36 @@
+use crate::{config, Point3, Ray, Vec3};
+
+pub struct Camera {
+    origin: Point3,
+    horizontal: Vec3,
+    vertical: Vec3,
+    lower_left_corner: Point3,
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        let viewport_height = 2.0;
+        let viewport_width = config::ASPECT_RATIO * viewport_height;
+        let focal_length = 1.0;
+
+        let origin = Point3::default();
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner =
+            origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+        Self {
+            origin,
+            horizontal,
+            vertical,
+            lower_left_corner,
+        }
+    }
+
+    pub fn ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+            lower_left_corner + horizontal * u + vertical * v - origin,
+        )
+    }
+}
