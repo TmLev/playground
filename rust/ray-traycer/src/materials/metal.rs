182f511417201e8d2a0eb9f82bbf869b1f4ef360
TmLev <sololev27@gmail.com>
2021-08-03 19:42:47 +0300

    feat: Fuzz `Metal`

--- rust/ray-traycer/src/materials/metal.rs
+++ rust/ray-traycer/src/materials/metal.rs
@@ -1,19 +1,26 @@
-use crate::{Color, HitRecord, Material, Ray, ScatterResult};
+use crate::{Color, HitRecord, Material, Ray, ScatterResult, Vec3};
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Self {
+            albedo,
+            fuzz: fuzz.min(1.0),
+        }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, ray: &Ray, record: &HitRecord) -> ScatterResult {
         let reflected = ray.direction().to_unit().reflect(record.normal());
-        let scattered = Ray::new(*record.point(), reflected);
+        let scattered = Ray::new(
+            *record.point(),
+            reflected + self.fuzz * Vec3::random_in_unit_sphere(),
+        );
         ScatterResult {
             attenuation: Some(self.albedo),
             scattered: Some(scattered),
