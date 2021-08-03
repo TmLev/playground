7a5e45fbbf03620f0fa79f80819f24e847befe6e
TmLev <sololev27@gmail.com>
2021-08-03 21:16:24 +0300

    cfg: Bump config

--- rust/ray-traycer/src/config.rs
+++ rust/ray-traycer/src/config.rs
@@ -1,10 +1,10 @@
 pub const OUTPUT_PATH: &str = "image.ppm";
 
-pub const ASPECT_RATIO: f64 = 16.0 / 9.0;
+pub const ASPECT_RATIO: f64 = 3.0 / 2.0;
 
-pub const IMAGE_WIDTH: usize = 400;
+pub const IMAGE_WIDTH: usize = 1200;
 pub const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as usize;
 
-pub const SAMPLES_PER_PIXEL: usize = 100;
+pub const SAMPLES_PER_PIXEL: usize = 500;
 
 pub const MAX_DEPTH: usize = 50;
