9928fe59455db825101c26ead7c2b5c488f0aa79
TmLev <sololev27@gmail.com>
2021-08-03 15:17:37 +0300

    refactor: Implement arithmetic ops with macros

--- rust/ray-traycer/src/vec.rs
+++ rust/ray-traycer/src/vec.rs
@@ -68,6 +68,11 @@ impl Vec3 {
         self.length_squared().sqrt()
     }
 
+    pub fn near_zero(&self) -> bool {
+        const EPS: f64 = 1e-8;
+        self[0].abs() < EPS && self[1].abs() < EPS && self[2].abs() < EPS
+    }
+
     // Operations
 
     pub fn to_unit(&self) -> Self {
@@ -92,6 +97,10 @@ impl Vec3 {
             ],
         }
     }
+
+    pub fn reflect(&self, normal: &Self) -> Self {
+        *self - 2.0 * self.dot(normal) * normal
+    }
 }
 
 // Indexing
@@ -110,91 +119,6 @@ impl IndexMut<usize> for Vec3 {
     }
 }
 
-// Arithmetic operations
-
-impl Neg for Vec3 {
-    type Output = Self;
-
-    fn neg(self) -> Self::Output {
-        Self {
-            coordinates: [-self[0], -self[1], -self[2]],
-        }
-    }
-}
-
-impl AddAssign<Self> for Vec3 {
-    fn add_assign(&mut self, rhs: Self) {
-        for (c, rc) in self.coordinates.iter_mut().zip(rhs.coordinates.iter()) {
-            *c += *rc;
-        }
-    }
-}
-
-impl Add<Self> for Vec3 {
-    type Output = Self;
-
-    fn add(mut self, rhs: Self) -> Self::Output {
-        self += rhs;
-        self
-    }
-}
-
-impl SubAssign<Self> for Vec3 {
-    fn sub_assign(&mut self, rhs: Self) {
-        *self += -rhs;
-    }
-}
-
-impl Sub<Self> for Vec3 {
-    type Output = Self;
-
-    fn sub(mut self, rhs: Self) -> Self::Output {
-        self += -rhs;
-        self
-    }
-}
-
-impl MulAssign<f64> for Vec3 {
-    fn mul_assign(&mut self, scalar: f64) {
-        self.coordinates.iter_mut().for_each(|x| *x *= scalar);
-    }
-}
-
-impl Mul<f64> for Vec3 {
-    type Output = Self;
-
-    fn mul(mut self, scalar: f64) -> Self::Output {
-        self *= scalar;
-        self
-    }
-}
-
-impl Mul<Self> for Vec3 {
-    type Output = Self;
-
-    fn mul(mut self, rhs: Self) -> Self::Output {
-        for (c, rc) in self.coordinates.iter_mut().zip(rhs.coordinates.iter()) {
-            *c *= *rc;
-        }
-        self
-    }
-}
-
-impl DivAssign<f64> for Vec3 {
-    fn div_assign(&mut self, scalar: f64) {
-        *self *= 1f64 / scalar;
-    }
-}
-
-impl Div<f64> for Vec3 {
-    type Output = Self;
-
-    fn div(mut self, scalar: f64) -> Self::Output {
-        self /= scalar;
-        self
-    }
-}
-
 // Utility
 
 impl Display for Vec3 {
@@ -202,3 +126,215 @@ impl Display for Vec3 {
         write!(f, "{} {} {}", self[0], self[1], self[2])
     }
 }
+
+// Arithmetic operations
+
+macro_rules! impl_bin_ops {
+    // `impl Add<(Vec or f64)> for (f64 or Vec)`
+    // with all possible combinations of references.
+    ($Vec:ident $Op:ident $op_fn:ident $op_symbol:tt) => {
+        // Base implementation for Vec + Vec.
+        // Implementations below forward operations here.
+        // e.g. a + b,
+        // where
+        //     a: &Vec,
+        //     b: &Vec,
+        impl<'a, 'other> $Op<&'other $Vec> for &'a $Vec {
+            type Output = $Vec;
+
+            fn $op_fn(self, other: &'other $Vec) -> Self::Output {
+                $Vec {
+                    coordinates: [
+                        self[0] $op_symbol other[0],
+                        self[1] $op_symbol other[1],
+                        self[2] $op_symbol other[2],
+                    ],
+                }
+            }
+        }
+
+        // e.g. a + b,
+        // where
+        //     a: Vec,
+        //     b: &Vec,
+        impl<'other> $Op<&'other $Vec> for $Vec {
+            type Output = $Vec;
+
+            fn $op_fn(self, other: &'other $Vec) -> Self::Output {
+                &self $op_symbol other
+            }
+        }
+
+        // e.g. a + b,
+        // where
+        //     a: &Vec,
+        //     b: Vec,
+        impl<'a> $Op<$Vec> for &'a $Vec {
+            type Output = $Vec;
+
+            fn $op_fn(self, other: $Vec) -> Self::Output {
+                self $op_symbol &other
+            }
+        }
+
+        // e.g. a + b,
+        // where
+        //     a: Vec,
+        //     b: Vec,
+        impl $Op<$Vec> for $Vec {
+            type Output = $Vec;
+
+            fn $op_fn(self, other: $Vec) -> Self::Output {
+                &self $op_symbol &other
+            }
+        }
+
+        // Base implementation for (Vec or f64) + (f64 or Vec).
+        // Implementations below forward operations here.
+        // e.g. a + b,
+        // where
+        //     a: &Vec,
+        //     b: f64,
+        impl<'a> $Op<f64> for &'a $Vec {
+            type Output = $Vec;
+
+            fn $op_fn(self, scalar: f64) -> Self::Output {
+                $Vec {
+                    coordinates: [
+                        self[0] $op_symbol scalar,
+                        self[1] $op_symbol scalar,
+                        self[2] $op_symbol scalar,
+                    ],
+                }
+            }
+        }
+
+        // e.g. a + b,
+        // where
+        //     a: f64,
+        //     b: &Vec,
+        impl<'other> $Op<&'other $Vec> for f64 {
+            type Output = $Vec;
+
+            fn $op_fn(self, other: &'other $Vec) -> Self::Output {
+                other $op_symbol self
+            }
+        }
+
+        // e.g. a + b,
+        // where
+        //     a: Vec,
+        //     b: f64,
+        impl $Op<f64> for $Vec {
+            type Output = $Vec;
+
+            fn $op_fn(self, scalar: f64) -> Self::Output {
+                &self $op_symbol scalar
+            }
+        }
+
+        // e.g. a + b,
+        // where
+        //     a: f64,
+        //     b: Vec,
+        impl $Op<$Vec> for f64 {
+            type Output = $Vec;
+
+            fn $op_fn(self, other: $Vec) -> Self::Output {
+                &other $op_symbol self
+            }
+        }
+    };
+}
+
+macro_rules! impl_assign_ops {
+    // `impl AddAssign<Vec> for Vec`
+    // with all possible combinations of references.
+    ($Vec:ident $Op:ident $op_fn:ident $op_symbol:tt) => {
+        // Base implementation for Vec += Vec.
+        // Implementations below forward operations here.
+        // e.g. a += b,
+        // where
+        //     a: &mut Vec,
+        //     b: &Vec,
+        impl<'other> $Op<&'other $Vec> for $Vec {
+            fn $op_fn(&mut self, other: &'other $Vec) {
+                *self = $Vec {
+                    coordinates: [
+                        self[0] $op_symbol other[0],
+                        self[1] $op_symbol other[1],
+                        self[2] $op_symbol other[2],
+                    ],
+                }
+            }
+        }
+
+        // e.g. a += b,
+        // where
+        //     a: &mut Vec,
+        //     b: Vec,
+        impl $Op for $Vec {
+            fn $op_fn(&mut self, other: $Vec) {
+                *self = *self $op_symbol other
+            }
+        }
+
+        // e.g. a += b,
+        // where
+        //     a: &mut Vec,
+        //     b: f64,
+        impl $Op<f64> for $Vec {
+            fn $op_fn(&mut self, other: f64) {
+                *self = *self $op_symbol other
+            }
+        }
+    };
+}
+
+macro_rules! impl_un_ops {
+    // `impl Neg for Vec`
+    // with all possible combinations of references.
+    ($Vec:ident $Op:ident $op_fn:ident $op_symbol:tt) => {
+        // Base implementation for -Vec.
+        // Implementations below forward operations here.
+        // e.g. -a
+        // where
+        //     a: &Vec,
+        impl<'a> $Op for &'a $Vec {
+            type Output = $Vec;
+
+            fn $op_fn(self) -> Self::Output {
+                $Vec {
+                    coordinates: [
+                        $op_symbol self[0],
+                        $op_symbol self[1],
+                        $op_symbol self[2],
+                    ],
+                }
+            }
+        }
+
+        // e.g. -a
+        // where
+        //     a: Vec,
+        impl $Op for $Vec {
+            type Output = $Vec;
+
+            fn $op_fn(self) -> Self::Output {
+                $op_symbol &self
+            }
+        }
+    };
+}
+
+impl_bin_ops!(Vec3 Add add +);
+impl_bin_ops!(Vec3 Sub sub -);
+impl_bin_ops!(Vec3 Mul mul *);
+impl_bin_ops!(Vec3 Div div /);
+
+impl_assign_ops!(Vec3 AddAssign add_assign +);
+impl_assign_ops!(Vec3 SubAssign sub_assign -);
+impl_assign_ops!(Vec3 MulAssign mul_assign *);
+impl_assign_ops!(Vec3 DivAssign div_assign /);
+
+impl_un_ops!(Vec3 Neg neg -);
