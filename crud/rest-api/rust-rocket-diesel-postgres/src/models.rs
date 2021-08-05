0b821c1a6d1bf6ff35dbed87203a8f2ea5b7eb16
TmLev <sololev27@gmail.com>
2021-08-05 14:23:56 +0300

    feat: Add `NewArtist` model

--- crud/rest-api/rust-rocket-diesel-postgres/src/models.rs
+++ crud/rest-api/rust-rocket-diesel-postgres/src/models.rs
@@ -1,9 +1,21 @@
-use rocket::serde::Serialize;
+use rocket::serde::{Deserialize, Serialize};
+
+use diesel::{Insertable, Queryable};
+
+use crate::schema::artists;
 
 #[derive(Serialize, Queryable, Debug)]
 #[serde(crate = "rocket::serde")]
 pub struct Artist {
     pub id: i32,
+    pub name: String,
+    pub description: String,
+}
+
+#[derive(Deserialize, Insertable, Debug)]
+#[serde(crate = "rocket::serde")]
+#[table_name = "artists"]
+pub struct NewArtist {
     pub name: String,
     pub description: String,
 }
